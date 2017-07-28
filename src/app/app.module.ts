4642fc4e9cf36093fba92942c8fd1079454372bf
Doroteya Nikolova <doroteanikolova93@gmail.com>
2017-07-28 18:01:05 +0300

    add login component,  add bootstrap and jquery in angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,12 @@ import { AppComponent } from './app.component';
 import { PostsComponent } from './posts/posts.component';
 
 import { PostsService } from './core/service/posts.service';
+import { LoginComponent } from './login/login.component';
 @NgModule({
   declarations: [
     AppComponent,
-    PostsComponent
+    PostsComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
